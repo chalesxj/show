887fac55c94cc4f2fa8dc563fe9090b06127ecb3
charlesxj <charles_xj@hotmail.com>
2016-09-14 16:42:09 +0800

    测试提交

---
+++ git_test/git_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git_test.Startup))]
+namespace git_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
